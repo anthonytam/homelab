64672ac43154876d2f336a35c1e88e2557164212
Anthony Tam <github@anthonytam.me>
2025-12-26 08:52:59 -0500

    Move cilium install

--- kubernetes/talos/node-labels.sh
+++ kubernetes/talos/node-labels.sh
@@ -10,3 +10,9 @@ kubectl label nodes worker05 topology.kubernetes.io/region=TamLand
 kubectl label nodes worker05 topology.kubernetes.io/zone=hype01
 kubectl label nodes controlplane03 topology.kubernetes.io/region=TamLand
 kubectl label nodes controlplane03 topology.kubernetes.io/zone=hype01
+
+kubectl label nodes worker01 egress-gw-vpn=true
+kubectl label nodes worker02 egress-gw-vpn=true
+kubectl label nodes worker03 egress-gw-vpn=true
+kubectl label nodes worker04 egress-gw-vpn=true
+kubectl label nodes worker05 egress-gw-vpn=true
