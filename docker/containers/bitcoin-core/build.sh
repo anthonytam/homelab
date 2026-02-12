e6617d6264794e4b219596c96714258541780405
Anthony Tam <github@anthonytam.me>
2026-02-12 01:36:01 -0500

    Updated docker builds

--- docker/containers/bitcoin-core/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -t "ghcr.io/anthonytam/bitcoind:v$1" --build-arg VERSION=$1 .
--- docker/containers/electrs/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -t "ghcr.io/anthonytam/electrs:v$1" --build-arg VERSION=$1 .
